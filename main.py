087c480a4a4b9dbeb7649122287f9fc9052c9143
Ali Marasizadeh <marasiali@gmail.com>
2020-05-26 12:47:23 +0430

    Find several optimize schedule (ONE-DAY-MODE)

--- main.py
+++ main.py
@@ -32,12 +32,29 @@ n = len(classes)
 # shift 1 item to right to change index to 1-BASED (start index from 1)
 classes.insert(0, None)
 
-unit_select_count = [0]
+# each item in format (unit_select_count, selected_units_indexes)
+unit_select = [ [0, []] ]
 for i in range(1, n+1):
     units = classes[i].units
+    units_paths = [ [i] ]
     last_non_conflict_index = last_non_conflict(classes, i)
     if last_non_conflict_index:
-        units += unit_select_count[last_non_conflict_index]
-    unit_select_count.append(max(units, unit_select_count[i-1]))
+        units += unit_select[last_non_conflict_index][0]
+        units_paths = unit_select[last_non_conflict_index][1]
+        if len(units_paths) > 0:
+            units_paths = list(map( lambda item: item + [i], units_paths))
+        else:
+            units_paths.append([i])
+    if units > unit_select[i-1][0]:
+        unit_select.append([units, units_paths])
+    elif units == unit_select[i-1][0]:
+        unit_select.append([ units, units_paths + unit_select[i-1][1] ])
+    else:
+        # [:] used to copy list
+        unit_select.append(unit_select[i-1][:])
 
-print(unit_select_count[n])
+print(unit_select[n][0])
+for i, schedule in enumerate(unit_select[n][1]):
+    print('\nSCHEDULE ({}) :'.format(i+1))
+    for item in schedule:
+        print(classes[item])
